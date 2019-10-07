ac68081e6591b3f82ee33cf5314e4895d9dbd60b
Lundii <mondayemmanuel67@gmail.com>
2019-10-07 03:21:55 +0100

    Updates

---
+++ precache-manifest.d4cd168d485fe384dfa12c20ecbbbcc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3dd50c615324711f12a595fcd2ddb44",
+    "url": "/address-app/index.html"
+  },
+  {
+    "revision": "a86fedbc52f69f212ab6",
+    "url": "/address-app/static/js/2.0728206b.chunk.js"
+  },
+  {
+    "revision": "aa7da0e0e797a0481056",
+    "url": "/address-app/static/js/main.bd5b9d8a.chunk.js"
+  },
+  {
+    "revision": "2343b9482468c696742d",
+    "url": "/address-app/static/js/runtime-main.926ab99b.js"
+  },
+  {
+    "revision": "36e2c09648b1492a6033d57f63a1fbbe",
+    "url": "/address-app/static/media/mapimg4.36e2c096.jpg"
+  },
+  {
+    "revision": "5de1602a7c9779ace28393b3cc8a34b7",
+    "url": "/address-app/static/media/mapimg5.5de1602a.png"
+  }
+]);
